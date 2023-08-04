3c58f06130ad44e2114d714294f2d1bb0efc474b
Anitha-2001 <anithavasanth42@gmail.com>
2023-08-04 10:58:00 +0100

    adding one program

---
+++ Pattern1.java
@@ -0,0 +1,20 @@
+package pattern;
+
+import java.util.Scanner;
+
+public class Pattern1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a number");
+		int j = sc.nextInt();
+		if(j%2==0) {
+		System.out.println("j value is even");
+		}
+		else {
+			System.out.println("j value is odd");
+		}
+
+}
+}
